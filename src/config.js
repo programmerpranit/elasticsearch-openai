7f5d403e3236bc5b2ec1834be266099e7ca0acb1
Pranit <pranit@webxstudio.in>
2023-08-04 18:43:20 +0530

    changed config file

--- src/config.js
+++ src/config.js
@@ -1,4 +1,4 @@
 export const BASE_URL =
   process.env.NODE_ENV === "production"
-    ? "https://next-gpt.vercel.app"
+    ? "https://elasticsearch-openai.vercel.app"
     : "http://localhost:3000";
